2595d290447a21c11b4181ae4790d54dba5c95e0
Rami Ilaiwi <rilaiwi@restaurant365.com>
2023-11-17 18:31:30 +0200

    Add project files.

---
+++ Domain/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagementSystem.Domain
+{
+    public class Inventory
+    {
+        private List<Product> products = new List<Product>();
+
+        public void AddProduct(string name, float price, int quantity)
+        {
+            Product product = new Product(name, price, quantity);
+            products.Add(product);
+        }
+    }
+}
---
+++ Domain/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagementSystem.Domain
+{
+    public class Product
+    {
+        public Product(string name, float price, int quantity) { 
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
